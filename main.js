fc973e6dde88ac8a0f88f3cc68db42db74af43b3
MohdNoorZ <88231703+MohdNoorZ@users.noreply.github.com>
2021-07-31 16:07:31 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 canvas = document.getElementById("myCanvas");
 color = "red";
 ctx= canvas.getContext("2d");
+ctx.beginPath();
+  ctx.strokeStyle = color;
+  ctx.lineWidth = 2;
+  ctx.arc(200,200,40,0,2 * Math.PI);
+  ctx.stroke();
+
 canvas.addEventListener("mousedown", my_mousedown);
 function my_mousedown(e)
 {
